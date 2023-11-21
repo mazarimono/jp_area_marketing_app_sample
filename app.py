b04d7edf5b0d0556d07a0bfb349b2940b40c1a71
mazarimono <mazarimono@gmail.com>
2023-11-21 10:44:15 +0900

    add area select

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import plotly.figure_factory as ff
 import streamlit as st
+from shapely.geometry import Point
+from toflat import toflat
 
 st.set_page_config(layout="wide")
 
@@ -45,9 +47,18 @@ def make_fig(
     selected_col: str,
     n_hex: int,
     iryo: bool,
-):
+    area_set: bool,
+    _df3: gpd.GeoDataFrame = None,
+    lon_fl: float = None,
+    lat_fl: float = None
+    ):
     """
     グラフ作成用関数
+    _df: 
+        主となるタイルのデータ
+    _df2:
+        医療機関のデータ
+    
     """
     _df["centroid_lat"] = _df["geometry"].map(lambda x: x.centroid.y)
     _df["centroid_lon"] = _df["geometry"].map(lambda x: x.centroid.x)
@@ -61,22 +72,38 @@ def make_fig(
         agg_func=np.mean,
         mapbox_style="open-street-map",
         color_continuous_scale="Viridis",
-        width=1200,
+        width=900,
         height=800,
     )
+    fig.update_layout(mapbox=dict(pitch=50))
     if iryo:
         fig.add_scattermapbox(
             lat=_df2.geometry.y,
             lon=_df2.geometry.x,
             marker={"color": "green", "opacity": 0.8},
         )
+    if area_set:
+        fig.add_scattermapbox(
+            lat=[lat_fl],
+            lon=[lon_fl],
+            marker={'color': 'blue', 'size': 30}
+        )
+        poly_geo = _df3.loc[1, 'geometry']
+        lons, lats = poly_geo.exterior.coords.xy
+        lons = list(lons)
+        lats = list(lats)
+        fig.add_scattermapbox(
+            mode='lines',
+            lon=lons,
+            lat=lats,
+            line={'width': 5}
+        )
     return fig
 
 
 # Views
 
-st.title("Chomoku App: Sample")
-
+# sidebar view
 with st.sidebar:
     option = st.selectbox(label="表示データ選択", options=list(data_dict.keys()))
     data_path = data_dict[option]
@@ -88,14 +115,38 @@ with st.sidebar:
     sel_ops = sel_cols(option, df)
     select_col = st.selectbox(label="表示データ選択", options=sel_ops)
     iryo = st.checkbox("医療機関データ")
+    # 商圏選択の実装
+    area_set = st.checkbox("エリア指定")
+    if area_set:
+        lon_fl = st.slider(label='経度選択', min_value=135.570, max_value=135.870, step=0.001, value=135.760)
+        lat_fl = st.slider(label='緯度選択', min_value=34.89, max_value=35.2, step=0.01, value=35.0)
+        market_size = st.slider(label='エリアサイズ（m）', min_value=500, max_value=5000, step=1, value=1000)
+        select_point = Point(lon_fl, lat_fl)
+        sel_gdf = gpd.GeoDataFrame(geometry=[select_point], crs='EPSG:4326')
+        tf = toflat.ToFlat(sel_gdf)
+        sel_gdf = tf.to_flat()
+        buff = sel_gdf.loc[0, 'geometry'].buffer(market_size)
+        sel_gdf.loc[1, 'geometry'] = buff
+        sel_gdf = sel_gdf.to_crs('EPSG:4326')
 
-fig = make_fig(df, iryo_df, select_col, n_hex, iryo)
+
+
+
+# site main view
+st.header("長目: エリアマーケティングアプリ")
+st.subheader("（サンプル: 京都市のみ）")
+# graph
+if area_set:
+    fig = make_fig(df, iryo_df, select_col, n_hex, iryo, area_set, sel_gdf, lon_fl, lat_fl)
+else:
+    fig = make_fig(df, iryo_df, select_col, n_hex, iryo, area_set)
 st.plotly_chart(fig)
 
 if st.checkbox("show data"):
     st.write(df)
 
-if st.checkbox("Data Sources: "):
+
+if st.checkbox("データ出典: "):
     st.write(
         "人口推計、地価評価、医療機関: [国土数値情報（国交省）](https://nlftp.mlit.go.jp/)を基に[合同会社長目](https://chomoku.info)が作成"
     )
